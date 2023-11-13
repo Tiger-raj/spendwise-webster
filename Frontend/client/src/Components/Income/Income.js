b0dfe8a25b5d5b29aed840e81273c8b207111b2d
Mishti Keshari <127041314+Mishtilorem@users.noreply.github.com>
2023-11-13 16:04:46 +0530

    Update Income.js

--- Frontend/client/src/Components/Income/Income.js
+++ Frontend/client/src/Components/Income/Income.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
 
+function Income() {
+    return (
+        <IncomeStyled></IncomeStyled>
+    )
+}
+
+const IncomeStyled = styled.div`
+
+
+`;
+
+export default Income
