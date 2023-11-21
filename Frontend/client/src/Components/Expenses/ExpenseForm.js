dda72ecb2ca466e7fcc46d5a4dcfdc84e51990db
DipanshiTyagi <143926420+DipanshiTyagi@users.noreply.github.com>
2023-11-21 19:22:18 +0530

    Create ExpenseForm.js

---
+++ Frontend/client/src/Components/Expenses/ExpenseForm.js
@@ -0,0 +1,176 @@
+// Import necessary modules and components
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import { useGlobalContext } from '../../context/globalContext';
+import Button from '../Button/Button';
+import { plus } from '../../utils/icons';
+
+
+function ExpenseForm() {
+
+    // Accessing required functions and states from global context
+    const {addExpense, error, setError} = useGlobalContext()
+
+    // State variables to manage form inputs
+    const [inputState, setInputState] = useState({
+        title: '',
+        amount: '',
+        date: '',
+        category: '',
+        description: '',
+    })
+
+
+    // Destructuring state variables for easier access
+    const { title, amount, date, category,description } = inputState;
+
+   // Function to handle input changes
+    const handleInput = name => e => {
+        setInputState({...inputState, [name]: e.target.value})
+        setError('')
+    }
+
+
+    // Function to handle form submission
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        // Calling addExpense function with form data
+        addExpense(inputState)
+
+        // Resetting the input state after submission
+        setInputState({
+            title: '',
+            amount: '',
+            date: '',
+            category: '',
+            description: '',
+        })
+    }
+
+    return (
+        // Styled form component to input expense details
+        <ExpenseFormStyled onSubmit={handleSubmit}>
+            {error && <p className='error'>{error}</p>}
+
+            {/* Input field for expense title */}
+            <div className="input-control">
+                <input 
+                    type="text" 
+                    value={title}
+                    name={'title'} 
+                    placeholder="Expense Title"
+                    onChange={handleInput('title')}
+                />
+            </div>
+
+            {/* Input field for expense amount */}
+            <div className="input-control">
+                <input value={amount}  
+                    type="text" 
+                    name={'amount'} 
+                    placeholder={'Expense Amount'}
+                    onChange={handleInput('amount')} 
+                />
+            </div>
+
+            {/* Date picker to select a date */}
+            <div className="input-control">
+                <DatePicker 
+                    id='date'
+                    placeholderText='Enter A Date'
+                    selected={date}
+                    dateFormat="dd/MM/yyyy"
+                    onChange={(date) => {
+                        setInputState({...inputState, date: date})
+                    }}
+                />
+            </div>
+
+            {/* Dropdown to select an expense category */}
+            <div className="selects input-control">
+                <select required value={category} name="category" id="category" onChange={handleInput('category')}>
+
+                    {/* Options for different expense categories */}
+                    <option value="" disabled >Select Option</option>
+                    <option value="education">Education</option>
+                    <option value="groceries">Groceries</option>
+                    <option value="health">Health</option>
+                    <option value="subscriptions">Subscriptions</option>
+                    <option value="takeaways">Takeaways</option>
+                    <option value="clothing">Clothing</option>  
+                    <option value="travelling">Travelling</option>  
+                    <option value="other">Other</option>  
+                </select>
+            </div>
+
+            {/* Textarea for description */}
+            <div className="input-control">
+                <textarea name="description" value={description} placeholder='Add A Reference' id="description" cols="30" rows="4" onChange={handleInput('description')}></textarea>
+            </div>
+
+            {/* Button to submit the form */}
+            <div className="submit-btn">
+                <Button 
+                    name={'Add Expense'}
+                    icon={plus}
+                    bPad={'.8rem 1.6rem'}
+                    bRad={'30px'}
+                    bg={'var(--color-accent'}
+                    color={'#fff'}
+                />
+            </div>
+        </ExpenseFormStyled>
+    )
+}
+
+// Styled form component
+const ExpenseFormStyled = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    input, textarea, select{
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: 5px;
+        border: 2px solid #fff;
+        background: transparent;
+        resize: none;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        color: rgba(34, 34, 96, 0.9);
+        &::placeholder{
+            color: rgba(34, 34, 96, 0.4);
+        }
+    }
+    .input-control{
+        input{
+            width: 100%;
+        }
+    }
+
+    .selects{
+        display: flex;
+        justify-content: flex-end;
+        select{
+            color: rgba(34, 34, 96, 0.4);
+            &:focus, &:active{
+                color: rgba(34, 34, 96, 1);
+            }
+        }
+    }
+
+    .submit-btn{
+        button{
+            box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+            &:hover{
+                background: var(--color-green) !important;
+            }
+        }
+    }
+`;
+export default ExpenseForm
