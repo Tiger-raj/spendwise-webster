ec8e65523819ac503ee0c77151585b440cb21fdd
DipanshiTyagi <143926420+DipanshiTyagi@users.noreply.github.com>
2023-11-23 18:23:06 +0530

    Update Layouts.js

--- Frontend/client/src/styles/Layouts.js
+++ Frontend/client/src/styles/Layouts.js
@@ -1,14 +1,19 @@
+//Importing the necessary method from the styled-components library
 import styled from "styled-components";
 
-export const MainLayout = styled.div`
 
+//Creating a styled component MainLayout using the styled.div method
+export const MainLayout = styled.div`
     padding: 2rem;
     height: 100%;
     display: flex;
     gap: 2rem; 
 `;
 
+
+//Creating another styled component InnerLayout using the styled.div method
 export const InnerLayout = styled.div`
     padding: 2rem 1.5rem;
     width: 100%;
+    
 `;
