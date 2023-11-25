7771193e78f38f0212b2e27eae28d628f6d451c9
DipanshiTyagi <143926420+DipanshiTyagi@users.noreply.github.com>
2023-11-25 09:51:03 +0530

    Update transactions.js

--- Backend/routes/transactions.js
+++ Backend/routes/transactions.js
@@ -1,14 +1,24 @@
+const { addExpense, getExpenses, deleteExpense  } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
-const { addExpense, getExpense, deleteExpense } = require('../controllers/expense');
-const router = require('express').Router()
+const { addTarget, getTarget, deleteTarget } = require('../controllers/target');
+
+
+
+const router = require('express').Router();
+
 
 
 router.post('/add-income', addIncome)
-    .get('/get-incomes', getIncomes)
-    .delete('/delete-income/:id', deleteIncome)
-    .post('/add-expense', addExpense)
-    .get('/get-expenses', getExpense)
-    .delete('/delete-expense/:id', deleteExpense)
-
+        .get('/get-incomes', getIncomes)
+        .delete('/delete-income/:id', deleteIncome)
+        .post('/add-expense', addExpense)
+        .get('/get-expenses', getExpenses)
+        .delete('/delete-expense/:id', deleteExpense)
+        .post('/add-target', addTarget)
+        .get('/get-target', getTarget)
+        .delete('/delete-target/:id', deleteTarget)
+        // .post('/add-bills', addBills)
+        // .get('/get-bills', getBills)
+        // .delete('/delete-target/:id', deleteTarget)
 
 module.exports = router
