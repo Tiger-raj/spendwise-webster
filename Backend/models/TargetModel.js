3794a666657d06c672c01d931add6f351889cd11
DipanshiTyagi <143926420+DipanshiTyagi@users.noreply.github.com>
2023-11-25 09:50:24 +0530

    Create TargetModel.js

---
+++ Backend/models/TargetModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const TargetSchema = new mongoose.Schema ({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    amount: {
+        type: Number,
+        required: true,
+        maxLength: 20,
+        trim: true
+    },
+    type: {
+        type: String,
+        default: "target"
+    },
+    date: {
+        type: Date,
+        required: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 20,
+        trim: true
+    },
+}, {timestamps: true})
+
+module.exports = mongoose.model('Target', TargetSchema)
